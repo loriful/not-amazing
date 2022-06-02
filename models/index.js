b67d5eb7a9bb8d4d67bc261ccde79b1b181411ef
loriful <tiptonica@gmail.com>
2022-06-02 00:47:56 -0400

    models done

--- models/index.js
+++ models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_name'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+foreignKey: 'product_id'
+});
+
 
 module.exports = {
   Product,
